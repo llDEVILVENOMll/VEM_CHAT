609f276542040181906eeb49bc6c787493cf04d7
VENOM <llDEVILVENOMll@gmail.com>
2025-02-21 10:42:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "7d1f14a0a818e651e147f2a95e560232"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7954204406p"))
+OWNER_ID = int(getenv("OWNER_ID", "7954204406"))
 SUPPORT_GRP = "FRIENSHIP_CLUB_GROUP"
 UPDATE_CHNL = "FEELING_SMILEY"
 OWNER_USERNAME = "I_a_m_venom"
